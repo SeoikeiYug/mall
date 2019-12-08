8aeb22d13b55b4748a9d78c1347bcba1b3d7305b
seoikei <seoikei@163.com>
2019-12-08 22:22:39 +0800

    mongodb整合

---
+++ src/main/java/com/genius/mall/controller/MemberReadHistoryController.java
@@ -0,0 +1,53 @@
+package com.genius.mall.controller;
+
+import com.genius.mall.common.api.CommonResult;
+import com.genius.mall.nosql.mongodb.document.MemberReadHistory;
+import com.genius.mall.service.MemberReadHistoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 会员商品浏览记录管理Controller
+ * Created by macro on 2019/12/08.
+ */
+@RestController
+@Api(tags = "MemberReadHistoryController", description = "会员商品浏览记录管理")
+@RequestMapping("/member/readHistory")
+public class MemberReadHistoryController {
+
+    @Autowired
+    private MemberReadHistoryService memberReadHistoryService;
+
+    @ApiOperation("创建浏览记录")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult create(@RequestBody MemberReadHistory memberReadHistory) {
+        int count = memberReadHistoryService.create(memberReadHistory);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("删除浏览记录")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public CommonResult delete(@RequestParam("ids") List<String> ids) {
+        int count = memberReadHistoryService.delete(ids);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("展示浏览记录")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<List<MemberReadHistory>> list(Long memberId) {
+        List<MemberReadHistory> memberReadHistoryList = memberReadHistoryService.list(memberId);
+        return CommonResult.success(memberReadHistoryList);
+    }
+}
